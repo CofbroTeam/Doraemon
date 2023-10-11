29971461b92cb3848659b8ec29d2f61408a84eb3
Frank <2876700951@qq.com>
2023-10-11 15:22:16 +0800

    textClass

---
+++ app/src/main/java/com/cofbro/qian/mapSetting/MapTextActivity2.kt
@@ -0,0 +1,12 @@
+package com.cofbro.qian.mapSetting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cofbro.qian.R
+
+class MapTextActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_text2)
+    }
+}
