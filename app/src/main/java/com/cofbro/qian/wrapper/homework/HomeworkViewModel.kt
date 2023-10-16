7acf10820e1cdfde9e6a48641c28d2207f0119bf
cofbro <1808413743@qq.com>
2023-10-16 23:52:11 +0800

    feat: 优化加载动画 & 移除测试代码

---
+++ app/src/main/java/com/cofbro/qian/wrapper/homework/HomeworkViewModel.kt
@@ -0,0 +1,23 @@
+package com.cofbro.qian.wrapper.homework
+
+import androidx.lifecycle.viewModelScope
+import com.cofbro.hymvvmutils.base.BaseViewModel
+import com.cofbro.hymvvmutils.base.ResponseMutableLiveData
+import com.cofbro.qian.utils.DefaultRepository
+import com.cofbro.qian.utils.NetworkUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.Response
+
+class HomeworkViewModel : BaseViewModel<DefaultRepository>() {
+    val homeworkLiveData = ResponseMutableLiveData<Response>()
+
+    fun queryHomeworkList(url: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.request(homeworkLiveData, false) {
+                val request = NetworkUtils.buildClientRequest(url)
+                NetworkUtils.request(request)
+            }
+        }
+    }
+}
