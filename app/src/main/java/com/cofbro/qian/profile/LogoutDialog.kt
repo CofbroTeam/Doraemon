bdb0f84ce3b603a323cbab485b65843c796a960b
Frank <2876700951@qq.com>
2023-10-19 13:58:38 +0800

    退出dialog界面,登出功能

--- app/src/main/java/com/cofbro/qian/profile/LogoutDialog.kt
+++ app/src/main/java/com/cofbro/qian/profile/LogoutDialog.kt
@@ -4,14 +4,32 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import com.amap.api.services.help.Tip
 import com.cofbro.qian.R
+import com.cofbro.qian.databinding.DialogLogoutBinding
 
 class LogoutDialog(context: Context):AlertDialog(context,R.style.Dialog_Fullscreen) {
+    lateinit var binding:DialogLogoutBinding
+    private var CancelClick: ((itemTip: String) -> Unit?)? = null
+    private var ConfirmClick: ((itemTip:String) -> Unit?)? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.dialog_logout)
+        binding = DialogLogoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         window?.setLayout(1000,1000)
+        binding.btnCancel.setOnClickListener {
+            CancelClick?.invoke("se")
+        }
+        binding.btnConfirm.setOnClickListener {
+            ConfirmClick?.invoke("se")
+        }
 
+    }
 
+     fun setCancelClickListener(CancelClickListener: (itemTip:String) -> Unit){
+            CancelClick = CancelClickListener
+    }
+     fun setConfirmClickListener(ConfirmClickListener: (itemTip:String) -> Unit){
+        ConfirmClick = ConfirmClickListener
     }
 }
--- app/src/main/java/com/cofbro/qian/profile/ProfileFragment.kt
+++ app/src/main/java/com/cofbro/qian/profile/ProfileFragment.kt
@@ -1,15 +1,20 @@
 package com.cofbro.qian.profile
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.ViewGroup.MarginLayoutParams
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.cofbro.hymvvmutils.base.BaseFragment
+import com.cofbro.hymvvmutils.base.saveUsedSp
 import com.cofbro.qian.data.URL
 import com.cofbro.qian.databinding.FragmentProfileBinding
+import com.cofbro.qian.login.LoginActivity
 import com.cofbro.qian.utils.CacheUtils
 import com.cofbro.qian.utils.dp2px
 import com.cofbro.qian.utils.getStatusBarHeight
@@ -53,17 +58,31 @@ class ProfileFragment : BaseFragment<ProfileViewModel, FragmentProfileBinding>()
             /**
              * 登出出现dialog
              */
-            viewModel.logout_dialog = LogoutDialog(requireContext())
-            viewModel.logout_dialog?.setCancelable(false)
-            viewModel.logout_dialog?.show()
-            lifecycleScope.launch(Dispatchers.IO) {
-                delay(2000)
-                withContext(Dispatchers.Main) {
+            viewModel.logout_dialog = LogoutDialog(requireContext()).apply {
+                this.setCancelClickListener {
                     viewModel.logout_dialog?.dismiss()
-
+                }
+                this.setConfirmClickListener {
+                    /**
+                     * dialog 清除数据中，并回到主登录界面
+                     */
+                    val intent = Intent(requireActivity(),LoginActivity::class.java)
+                    clearUserInfo(context)
+                    startActivity(intent)
+                    requireActivity().finish()
                 }
             }
+            viewModel.logout_dialog?.setCancelable(false)
+            viewModel.logout_dialog?.show()
         }
+        binding?.cowithprofiles?.setOnClickListener {
+
+        }
+    }
+    private fun clearUserInfo(context: Context){
+        context.saveUsedSp("username", "")
+        context.saveUsedSp("password", "")
+        Toast.makeText(context, "数据删除成功", Toast.LENGTH_SHORT).show()
     }
 
 }
