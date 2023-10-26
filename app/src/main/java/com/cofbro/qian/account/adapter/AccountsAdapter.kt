5ee628256b1acdf897eb78e6cc960930e0e98898
cofbro <1808413743@qq.com>
2023-10-26 20:24:38 +0800

    feat: add AccountManagerActivity.kt

--- app/src/main/java/com/cofbro/qian/account/adapter/AccountsAdapter.kt
+++ app/src/main/java/com/cofbro/qian/account/adapter/AccountsAdapter.kt
@@ -10,10 +10,11 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
-import com.cofbro.qian.account.manager.User
 import com.cofbro.qian.data.URL
+import com.cofbro.qian.databinding.ItemAccountsListBinding
 import com.cofbro.qian.databinding.ItemTaskListBinding
 import com.cofbro.qian.utils.dp2px
+import com.cofbro.qian.utils.getIntExt
 import com.cofbro.qian.utils.getJSONArrayExt
 import com.cofbro.qian.utils.getStringExt
 import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter
@@ -44,10 +45,11 @@ import jp.wasabeef.recyclerview.animators.holder.AnimateViewHolder
 class AccountsAdapter : RecyclerView.Adapter<AccountsAdapter.AccountsHolder>() {
     private var data: JSONObject? = null
     private var itemClick: ((JSONObject?) -> Unit)? = null
+    private var itemLongClick: ((JSONObject?, Int) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AccountsHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemTaskListBinding.inflate(inflater, parent, false)
+        val binding = ItemAccountsListBinding.inflate(inflater, parent, false)
         return AccountsHolder(binding)
     }
 
@@ -59,8 +61,9 @@ class AccountsAdapter : RecyclerView.Adapter<AccountsAdapter.AccountsHolder>() {
         holder.bind(position)
     }
 
-    inner class AccountsHolder(private val binding: ItemTaskListBinding) :
+    inner class AccountsHolder(private val binding: ItemAccountsListBinding) :
         RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("SetTextI18n")
         fun bind(position: Int) {
             val itemData = data?.getJSONArrayExt("users")?.getOrNull(position) ?: JSONObject()
             (itemData as? JSONObject).let { itemValue ->
@@ -71,32 +74,58 @@ class AccountsAdapter : RecyclerView.Adapter<AccountsAdapter.AccountsHolder>() {
                 // 绑定数据
                 val options = RequestOptions().transform(
                     CenterCrop(),
-                    RoundedCorners(dp2px(binding.root.context, 5))
+                    RoundedCorners(dp2px(binding.root.context, 25))
                 )
                 Glide.with(binding.root)
                     .load(picUrl)
                     .apply(options)
                     .into(binding.ivTaskImage)
-                binding.tvTitle.text = username
-                binding.tvTimeLine.text = uid
+                binding.tvTitle.text = "账号：$username"
+                binding.tvTimeLine.text = "uid：$uid"
 
                 itemView.setOnClickListener {
                     itemClick?.invoke(itemValue)
+                }
+
+                itemView.setOnLongClickListener {
+                    itemLongClick?.invoke(itemValue, position)
+                    false
                 }
             }
         }
     }
 
+    fun setItemOnLongClickListener(listener: (JSONObject?, Int) -> Unit) {
+        itemLongClick = listener
+    }
+
     @SuppressLint("NotifyDataSetChanged")
-    fun setData(t: JSONObject) {
+    fun setData(t: JSONObject?) {
         data = t
         notifyDataSetChanged()
     }
 
-    fun remove(position: Int) {
+    fun getData(): JSONObject? {
+        return data
+    }
+
+    fun remove(uid: String) {
+        var index = 0
         val array = data?.getJSONArrayExt("users")
-        array?.removeAt(position)
-        notifyItemRemoved(position)
+        array?.let {
+            it.forEachIndexed { i, any ->
+                val data = any as? JSONObject
+                if (data?.getStringExt("uid") == uid) {
+                    index = i
+                }
+            }
+            if (index < it.size) {
+                array.removeAt(index)
+                data?.set("size", array.size)
+                data?.set("users", array)
+                notifyItemRemoved(index)
+            }
+        }
     }
 
     fun add(text: String, position: Int) {
--- app/src/main/java/com/cofbro/qian/account/manager/AccountManagerActivity.kt
+++ app/src/main/java/com/cofbro/qian/account/manager/AccountManagerActivity.kt
@@ -2,7 +2,10 @@ package com.cofbro.qian.account.manager
 
 import android.graphics.Rect
 import android.os.Bundle
+import android.os.Vibrator
 import android.view.View
+import android.view.ViewGroup.MarginLayoutParams
+import android.view.animation.AnticipateOvershootInterpolator
 import androidx.core.widget.NestedScrollView
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,11 +22,13 @@ import com.cofbro.qian.utils.dp2px
 import com.cofbro.qian.utils.getIntExt
 import com.cofbro.qian.utils.getJSONArrayExt
 import com.cofbro.qian.utils.getStatusBarHeight
+import com.cofbro.qian.utils.getStringExt
 import com.cofbro.qian.utils.safeParseToJson
 import com.cofbro.qian.view.FullScreenDialog
+import com.cofbro.qian.view.TipDialog
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.hjq.toast.ToastUtils
-import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter
+import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter
 import jp.wasabeef.recyclerview.animators.ScaleInLeftAnimator
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -39,6 +44,7 @@ class AccountManagerActivity :
     private var loadingView: FullScreenDialog? = null
     private var mAdapter: AccountsAdapter? = null
     private var data: JSONObject? = null
+    private var toolbarHeight = 0
     private var mUsername = ""
     private var mPassword = ""
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -62,11 +68,30 @@ class AccountManagerActivity :
 
 
     private fun initView() {
-        mAdapter = AccountsAdapter()
-        mAdapter?.setData(data!!)
+        initToolbar()
+        binding?.csContent?.apply {
+            val height = resources.displayMetrics.heightPixels
+            val layout = layoutParams
+            layout.height = height
+            layoutParams = layout
+        }
+        mAdapter = AccountsAdapter().apply {
+            setItemOnLongClickListener { itemData, _ ->
+                vibrate()
+                showTipDialog(itemData)
+            }
+        }
+        mAdapter?.setData(data)
         binding?.recyclerView?.apply {
             itemAnimator = ScaleInLeftAnimator()
-            adapter = AlphaInAnimationAdapter(mAdapter!!)
+            adapter = ScaleInAnimationAdapter(mAdapter!!).apply {
+                // Change the durations.
+                setDuration(1000)
+                // Change the interpolator.
+                setInterpolator(AnticipateOvershootInterpolator())
+                // Disable the first scroll mode.
+                setFirstOnly(false)
+            }
             layoutManager = LinearLayoutManager(
                 this@AccountManagerActivity,
                 LinearLayoutManager.VERTICAL,
@@ -80,17 +105,13 @@ class AccountManagerActivity :
                     parent: RecyclerView,
                     state: RecyclerView.State
                 ) {
-                    val defaultPadding = dp2px(this@AccountManagerActivity, 15)
-//                    val toolbarHeight = binding?.appToolBar?.height ?: 0
-                    if (parent.layoutManager?.getPosition(view) == 0) {
-                        return outRect.set(
-                            defaultPadding,
-                            getStatusBarHeight(this@AccountManagerActivity) + dp2px(this@AccountManagerActivity, 5),
-                            defaultPadding,
-                            defaultPadding
-                        )
-                    }
-                    return super.getItemOffsets(outRect, view, parent, state)
+                    val defaultPadding = dp2px(this@AccountManagerActivity, 16)
+                    return outRect.set(
+                        defaultPadding,
+                        0,
+                        defaultPadding,
+                        dp2px(this@AccountManagerActivity, 12)
+                    )
                 }
             })
 
@@ -99,7 +120,7 @@ class AccountManagerActivity :
         binding?.bottomSheet?.let {
             behavior = BottomSheetBehavior.from(it)
             behavior?.apply {
-                isFitToContents = false //展开后开度填充Parent的高度
+                isFitToContents = false
                 expandedOffset = getStatusBarHeight(this@AccountManagerActivity) + 40
                 halfExpandedRatio = 0.5f
                 isHideable = false
@@ -112,6 +133,26 @@ class AccountManagerActivity :
         binding?.etUsername?.setOnFocusChangeListener { v, hasFocus ->
             if (hasFocus) {
                 behavior?.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+        }
+    }
+
+    private fun initToolbar() {
+        // height of toolbar
+        binding?.toolbar?.apply {
+            toolbarHeight = getStatusBarHeight(this@AccountManagerActivity) + dp2px(
+                this@AccountManagerActivity,
+                45
+            )
+            val csLayout = layoutParams
+            csLayout.height = toolbarHeight
+        }
+        // topMargin of recyclerView
+        binding?.recyclerView?.apply {
+            val rvLayout = layoutParams as? MarginLayoutParams
+            rvLayout?.let {
+                it.topMargin = toolbarHeight + dp2px(this@AccountManagerActivity, 5)
+                layoutParams = it
             }
         }
     }
@@ -131,9 +172,17 @@ class AccountManagerActivity :
 
     private fun initEvent() {
         binding?.tvBinding?.setOnClickListener {
-            showLoadingView()
-            login()
+            bindAccount()
         }
+
+        binding?.tvBack?.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun bindAccount() {
+        showLoadingView()
+        login()
     }
 
     private fun login() {
@@ -167,12 +216,19 @@ class AccountManagerActivity :
                     Constants.RecycleJson.ACCOUNT_JSON_DATA,
                     jsonObject?.toJSONString() ?: ""
                 )
+                ToastUtils.show("绑定成功！")
+                clearText()
                 hideLoadingView()
             } catch (_: Exception) {
             }
         } else {
             ToastUtils.show("绑定失败!请检查账号密码是否正确!")
         }
+    }
+
+    private fun clearText() {
+        binding?.etUsername?.text?.clear()
+        binding?.etPassword?.text?.clear()
     }
 
     private fun parseToJSONObject(uid: String, fid: String): JSONObject? {
@@ -183,6 +239,7 @@ class AccountManagerActivity :
         jsonObject["password"] = mPassword
         jsonObject["uid"] = uid
         jsonObject["fid"] = fid
+        jsonObject["picUrl"] = URL.getAvtarImgPath(uid)
         if (file.exists()) {
             val newSize = data?.getIntExt("size") ?: 0
             val array = data?.getJSONArrayExt("users") ?: JSONArray()
@@ -217,5 +274,39 @@ class AccountManagerActivity :
     private fun hideLoadingView() {
         loadingView?.dismiss()
         loadingView = null
+    }
+
+    private fun showTipDialog(itemData: JSONObject?) {
+        TipDialog(this).apply {
+            setPositiveClickListener {
+                dismiss()
+                val uid = itemData?.getStringExt("uid") ?: ""
+                mAdapter?.remove(uid)
+                updateAccountData()
+            }
+            setNegativeClickListener {
+                dismiss()
+            }
+            setCancelable(false)
+            show()
+        }
+    }
+
+    private fun updateAccountData() {
+        val data = mAdapter?.getData()?.toJSONString() ?: ""
+        lifecycleScope.launch(Dispatchers.IO) {
+            Downloader.download(
+                this@AccountManagerActivity,
+                Constants.RecycleJson.ACCOUNT_JSON_DATA,
+                data
+            )
+        }
+    }
+
+    private fun vibrate() {
+        val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
+        if (vibrator.hasVibrator()) {
+            vibrator.vibrate(100L)
+        }
     }
 }
