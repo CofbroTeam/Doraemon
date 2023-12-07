cf12df6c6d96e8cb5a1c0d794f5c92f8f377c1a0
cofbro <1808413743@qq.com>
2023-12-07 16:25:14 +0800

    complete cookie sign

--- app/src/main/java/com/cofbro/qian/friend/chat/viewholder/CookieSignCardSelfViewHolder.kt
+++ app/src/main/java/com/cofbro/qian/friend/chat/viewholder/CookieSignCardSelfViewHolder.kt
@@ -1,11 +1,20 @@
 package com.cofbro.qian.friend.chat.viewholder
 
+import android.opengl.Visibility
+import android.view.View
+import cn.leancloud.LCObject
 import cn.leancloud.im.v2.LCIMMessage
 import cn.leancloud.im.v2.messages.LCIMTextMessage
+import com.cofbro.qian.R
 import com.cofbro.qian.data.URL
 import com.cofbro.qian.databinding.ItemChatCookieSignCardMyselfBinding
+import com.cofbro.qian.friend.im.IMClientUtils
 import com.cofbro.qian.friend.viewholder.DefaultViewHolder
+import com.cofbro.qian.mapsetting.util.ToastUtil
 import com.cofbro.qian.utils.CacheUtils
+import com.cofbro.qian.utils.MsgFactory
+import com.cofbro.qian.view.WaitDialog
+import com.hjq.toast.ToastUtils
 
 class CookieSignCardSelfViewHolder(private val binding: ItemChatCookieSignCardMyselfBinding) :
     DefaultViewHolder<LCIMMessage>(binding) {
@@ -18,5 +27,58 @@ class CookieSignCardSelfViewHolder(private val binding: ItemChatCookieSignCardMy
         // 头像
         val avatar = URL.getAvtarImgPath(CacheUtils.cache["uid"] ?: "")
         setImage(binding.avatar, avatar)
+        // cookie
+        val textMsg = t as? LCIMTextMessage
+        val cookie =
+            textMsg?.attrs?.getOrDefault(MsgFactory.cookieSign, "").toString()
+        itemView.setOnClickListener {
+            bindAction(textMsg, cookie)
+        }
+    }
+
+    private fun bindAction(textMsg: LCIMTextMessage?, cookie: String) {
+        if (checkCookieIfValid(textMsg)) {
+            findCookieCardInfo(cookie) {
+                val agree = it?.get("agree").toString()
+                val content =
+                    getHolderContext().resources.getString(R.string.cookie_sign_wait_dialog_self_content)
+                val title = if (agree.isEmpty()) {
+                    getHolderContext().resources.getString(R.string.cookie_sign_wait_dialog_self_title_not_response)
+                } else if (agree == "agree") {
+                    getHolderContext().resources.getString(R.string.cookie_sign_wait_dialog_self_title_agree)
+                } else {
+                    getHolderContext().resources.getString(R.string.cookie_sign_wait_dialog_self_title_refuse)
+                }
+                showWaitDialog(title, content)
+            }
+        } else {
+            ToastUtils.show("卡片已过期~")
+        }
+    }
+
+    private fun findCookieCardInfo(cookie: String, callback: (LCObject?) -> Unit) {
+        val cardId = IMClientUtils.getCntUser()?.objectId + cookie
+        IMClientUtils.findCookieCardInfo(cardId, onSuccess = {
+            callback(it.getOrNull(0))
+        }, onError = {})
+    }
+
+    private fun showWaitDialog(title: String, content: String) {
+        WaitDialog(getHolderContext()).apply {
+            show()
+            setTitle(title)
+            setContent(content)
+            csBottom?.visibility = View.GONE
+            tipSureButton?.visibility = View.VISIBLE
+            setOnClickTipSureButton {
+                dismiss()
+            }
+        }
+    }
+
+    private fun checkCookieIfValid(msg: LCIMTextMessage?): Boolean {
+        val cardTime = msg?.timestamp ?: 0L
+        val nowTime = System.currentTimeMillis()
+        return nowTime - cardTime <= 24 * 60 * 60 * 1000
     }
 }
--- app/src/main/java/com/cofbro/qian/view/WaitDialog.kt
+++ app/src/main/java/com/cofbro/qian/view/WaitDialog.kt
@@ -7,13 +7,19 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.cofbro.qian.R
 
 class WaitDialog(context: Context) : AlertDialog(context) {
+    var csBottom: ConstraintLayout? = null
+    var tipSureButton: TextView? = null
     private var positiveButton: TextView? = null
     private var negativeButton: TextView? = null
+    private var title: TextView? = null
+    private var content: TextView? = null
     private var positiveClickListener: ((View) -> Unit)? = null
     private var negativeClickListener: ((View) -> Unit)? = null
+    private var tipSureClickListener: ((View) -> Unit)? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_wait)
@@ -28,6 +34,10 @@ class WaitDialog(context: Context) : AlertDialog(context) {
     private fun initView() {
         positiveButton = findViewById(R.id.tv_cookie_sign_positive)
         negativeButton = findViewById(R.id.tv_cookie_sign_negative)
+        tipSureButton = findViewById(R.id.tv_tip_sure)
+        csBottom = findViewById(R.id.cs_bottom)
+        title = findViewById(R.id.tv_cookie_sign_dialog_title)
+        content = findViewById(R.id.tv_cookie_sign_dialog_content)
     }
 
     private fun bindAction() {
@@ -38,6 +48,18 @@ class WaitDialog(context: Context) : AlertDialog(context) {
         negativeButton?.setOnClickListener {
             negativeClickListener?.invoke(it)
         }
+
+        tipSureButton?.setOnClickListener {
+            tipSureClickListener?.invoke(it)
+        }
+    }
+
+    fun setTitle(titleString: String) {
+        title?.text = titleString
+    }
+
+    fun setContent(contentString: String) {
+        content?.text = contentString
     }
 
     fun setOnClickPositiveButton(listener: (View) -> Unit) {
@@ -46,5 +68,9 @@ class WaitDialog(context: Context) : AlertDialog(context) {
 
     fun setOnClickNegativeButton(listener: (View) -> Unit) {
         negativeClickListener = listener
+    }
+
+    fun setOnClickTipSureButton(listener: (View) -> Unit) {
+        tipSureClickListener = listener
     }
 }
